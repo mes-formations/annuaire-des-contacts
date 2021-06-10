603e8f2914d1e8a4494bf3ab033f944d5c9e7752
Christian Lisangola <jochri3@gmail.com>
2021-06-10 10:17:37 +0100

    add contacts collection component

---
+++ src/components/contacts-collection/contacts-collection.component.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const ContactsCollection: React.FC = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Prénom</th>
+          <th>Email</th>
+          <th>Poste</th>
+          <th>Numéro de téléphone</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Expliquer code ES6 */}
+        {/* {contactsState[0].map((contact, index) => (
+          <tr key={contact.id}>
+            <td>{contact.last_name}</td>
+            <td>{contact.first_name}</td>
+            <td>{contact.email}</td>
+            <td>{contact.position}</td>
+            <td>{contact.phone_number}</td>
+            <td>
+              <button onClick={() => deleteContact(index)}>Supprimer</button>
+            </td>
+          </tr>
+        ))} */}
+      </tbody>
+    </table>
+  );
+};
+
+export default ContactsCollection;
