5f0cb5d9f6dbbd9fe5ecf10561dea4187f4c8b08
Christian Lisangola <jochri3@gmail.com>
2021-06-10 16:56:41 +0100

    check id passed as props

--- src/pages/contact/show/show.tsx
+++ src/pages/contact/show/show.tsx
@@ -1,5 +1,10 @@
-const ContactShow = () => {
-  return <h1>Bienvenu dans la page détails de contact</h1>;
+const ContactShow: React.FC = (props: any) => {
+  return (
+    <h1>
+      Bienvenu dans la page détails de contact, mon id est :{" "}
+      {props.match.params.id}
+    </h1>
+  );
 };
 
 export default ContactShow;
