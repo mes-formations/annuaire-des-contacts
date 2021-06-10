3842b8b3bcdec2d60cde2e7d65402731043649d4
Christian Lisangola <jochri3@gmail.com>
2021-06-10 17:14:16 +0100

    feat:implement UI for contact details

--- src/pages/contact/show/show.tsx
+++ src/pages/contact/show/show.tsx
@@ -1,14 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { RouteComponentProps } from "react-router-dom";
+import { IContact } from "../../../interfaces/i-contact";
 
 type ContactIdParam = { id: string };
 
 type ContactDetailsRouterProps = RouteComponentProps<ContactIdParam>;
 
 const ContactShow: React.FC<ContactDetailsRouterProps> = ({ match }) => {
+  const [contact, setContact] = useState<IContact | null>(null);
+  async function fetchContactById(id: string) {
+    const { data } = await axios
+      .get<IContact>(`http://localhost:3333/contacts/${id}`)
+      .then((res) => res);
+    return data;
+  }
+  useEffect(() => {
+    (async () => {
+      const contact = await fetchContactById(match.params.id).then(
+        (data) => data
+      );
+      setContact(contact);
+    })();
+  }, []);
+
   return (
-    <h1>
-      Bienvenu dans la page détails de contact, mon id est : {match.params.id}
-    </h1>
+    <>
+      <h1>Détails de contact</h1>
+      {contact && (
+        <>
+          <h2>
+            {contact.first_name} {contact.last_name}
+          </h2>
+          <h3>{contact.position}</h3>
+          <p>
+            Email : <a href={`mailto:${contact.email}`}>{contact.email}</a>
+          </p>
+          <p>
+            Téléphone :{" "}
+            <a href={`tel:${contact.phone_number}`}>{contact.phone_number}</a>
+          </p>
+          <p>Adresse : {contact.work_address}</p>
+        </>
+      )}
+    </>
   );
 };
 
