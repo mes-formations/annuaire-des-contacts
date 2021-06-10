ff96bf732cff5deea0d368039d6bac995f821cc0
Christian Lisangola <jochri3@gmail.com>
2021-06-10 09:58:14 +0100

    add error in row interatiob because data type is never, because promis may not be resolved

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,62 +1,28 @@
 import React from "react";
+import axios from "axios";
 //Inférence
-const contacts = [
-  {
-    _id: "607cf129311fcb0015dd6e89",
-    first_name: "Yukihiro",
-    last_name: "Matsumoto Fumbwa",
-    email: "Matsumoto@gmail.com",
-    phone_number: "+243811609403",
-    position: "Teacher and Software Engineer",
-    work_address: "Tokyo",
-  },
-  {
-    _id: "607cf19e311fcb0015dd6e8b",
-    first_name: "Linus",
-    last_name: "Torvalds",
-    email: "linux@gmail.com",
-    phone_number: "+33878578477",
-    position: "Software Engineer",
-    work_address: "Helsinki",
-  },
-  {
-    _id: "6091374a5516790015430f3a",
-    first_name: "Vance",
-    last_name: "Carney",
-    email: "zygave@mailinator.com",
-    phone_number: "+243818636535",
-    position: "Sint voluptatem dist",
-    work_address: "Veniam eaque offici",
-  },
-  {
-    _id: "60914868c3c58500158f9927",
-    first_name: "Zelda",
-    last_name: "Wolf",
-    email: "ziky@mailinator.com",
-    phone_number: "+243819621840",
-    position: "Aliquam deserunt pra",
-    work_address: "Debitis dolorem dolo",
-  },
-  {
-    _id: "6091487dc3c58500158f9928",
-    first_name: "Thomas",
-    last_name: "Kim",
-    email: "geron@mailinator.com",
-    phone_number: "+243810884055",
-    position: "Adipisci deserunt ar",
-    work_address: "Minima enim dolore i",
-  },
-];
 
 const App: React.FC = () => {
-  const contactsState = React.useState(contacts);
-
+  const contactsState = React.useState([]);
   // Mise en place du typage static
+  async function fetchContacts() {
+    const contacts = await axios
+      .get("http://localhost:3333/contacts")
+      .then((res) => res.data);
+    return contacts;
+  }
   const deleteContact = (index: number) => {
     const newState = [...contactsState[0]];
     newState.splice(index, 1);
     contactsState[1](newState);
   };
+
+  React.useEffect(() => {
+    (async () => {
+      const contacts = await fetchContacts().then((data) => data);
+      contactsState[1](contacts);
+    })();
+  }, []);
   return (
     <table>
       <thead>
@@ -72,7 +38,7 @@ const App: React.FC = () => {
       <tbody>
         {/* Expliquer code ES6 */}
         {contactsState[0].map((contact, index) => (
-          <tr key={contact._id}>
+          <tr key={contact.id}>
             <td>{contact.last_name}</td>
             <td>{contact.first_name}</td>
             <td>{contact.email}</td>
