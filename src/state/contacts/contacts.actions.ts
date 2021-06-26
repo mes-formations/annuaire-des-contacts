64ec2208440b1c14838f02a1df7ad6f67c931eb0
Christian Lisangola <jochri3@gmail.com>
2021-06-26 08:32:53 +0100

    add contacts actions interfaces

---
+++ src/state/contacts/contacts.actions.ts
@@ -0,0 +1,19 @@
+import { IContact } from "../../interfaces/i-contact";
+import { ActionTypes } from "./contacts.action-types";
+
+/**
+ *  GET /contacts
+ */
+export interface IGetContactsRequest {
+  type: ActionTypes.GET_CONTACTS_REQUEST;
+}
+
+export interface IGetContactsSuccess {
+  type: ActionTypes.GET_CONTACTS_SUCESS;
+  payload: IContact[];
+}
+
+export interface IGetContactsFailure {
+  type: ActionTypes.GET_CONTACTS_FAILURE;
+  payload: string;
+}
