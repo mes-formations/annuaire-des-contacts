2a23886869d22bc33cbe2daae2de3608c8ce6860
Christian Lisangola <jochri3@gmail.com>
2021-06-26 07:53:42 +0100

    test working of redux insid home component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Home: React.FC = () => {
+  const test = useSelector<any>((state) => state); //Il fera abstraction getState,et les autres
+  console.log("Test : ", test);
   return (
     <h1>
       Welcome to the homepage,so see the contacts list clic{" "}
