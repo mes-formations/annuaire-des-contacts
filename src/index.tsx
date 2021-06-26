5d8dc4e0ebc31eabceae8f0e38ee71b217d279ef
Christian Lisangola <jochri3@gmail.com>
2021-06-26 07:41:22 +0100

    connect react and redux via Provider in the root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import ReactDOM from "react-dom";
 import "./sass/main.scss";
 import App from "./app";
+import store from "./state/store";
+import { Provider } from "react-redux";
 
 const root = document.querySelector("#root");
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  root
+);
