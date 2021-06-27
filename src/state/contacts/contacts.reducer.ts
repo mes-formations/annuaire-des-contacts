902e6cd7e430cdd03c3e5ce6a56da67a3e52c69d
Christian Lisangola <jochri3@gmail.com>
2021-06-27 21:14:28 +0100

    add contacts state interface + initial state setup

--- src/state/contacts/contacts.reducer.ts
+++ src/state/contacts/contacts.reducer.ts
@@ -1,2 +1,20 @@
 import { ActionTypes } from "./contacts.action-types";
 import { Action } from "./contacts.actions";
+import { IContact } from "../../interfaces/i-contact";
+
+interface IContactsState{
+    contacts:IContact[];
+    loading:boolean;
+    error:string | null;
+}
+
+const initialState:IContactsState={
+    contacts:[],
+    loading:false,
+    error:""
+}
+
+export default (state:IContactsState=initialState,action:Action){
+    return state;
+}
+
