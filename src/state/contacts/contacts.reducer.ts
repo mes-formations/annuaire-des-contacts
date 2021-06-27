127fb4ac1e66bec40342048f3a872efa2c041e06
Christian Lisangola <jochri3@gmail.com>
2021-06-27 23:12:40 +0100

    add cases for contacts reducer

--- src/state/contacts/contacts.reducer.ts
+++ src/state/contacts/contacts.reducer.ts
@@ -54,6 +54,18 @@ const initialState: IContactsState = {
 export const contactsReducer = (
   state: IContactsState = initialState,
   action: Action
-) => {
+): IContactsState => {
+  if (action.type === ActionTypes.GET_CONTACTS_REQUEST) {
+    return { ...state, loading: true, error: null, contacts: [] };
+  }
+
+  if (action.type === ActionTypes.GET_CONTACTS_SUCESS) {
+    return { ...state, loading: false, error: null, contacts: action.payload };
+  }
+
+  if (action.type === ActionTypes.GET_CONTACTS_FAILURE) {
+    return { ...state, loading: false, contacts: [], error: action.payload };
+  }
+
   return state;
 };
