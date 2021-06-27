68ba8202ddffae843fd4e6e5dcde9f9e945da06f
Christian Lisangola <jochri3@gmail.com>
2021-06-28 00:13:16 +0100

    cleanup unused vars

--- src/pages/contact/index/index.tsx
+++ src/pages/contact/index/index.tsx
@@ -1,11 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { IContact } from "../../../interfaces/i-contact";
 import ContactsCollection from "../../../components/contact/contacts-collection/contacts-collection";
-import contactApi from "../../../configs/contact.api";
 import "./style.scss";
 import { getContacts } from "../../../state/contacts/contacts.action-creators";
 import { useDispatch, useSelector } from "react-redux";
-import { IContactsState } from "../../../state/contacts/contacts.reducer";
 
 const ContactsList: React.FC = () => {
   const dispatch = useDispatch();
