feec15fca6ba0c54144dfa37ddfb9a7610d46285
Christian Lisangola <jochri3@gmail.com>
2021-06-29 20:53:55 +0100

    add GET_CONTACT action type

--- src/state/contacts/contacts.action-types.ts
+++ src/state/contacts/contacts.action-types.ts
@@ -1,5 +1,9 @@
 export enum ActionTypes {
+  // GET /api/contacts
   GET_CONTACTS_REQUEST = "get_contacts_request",
   GET_CONTACTS_SUCESS = "get_contacts_success",
   GET_CONTACTS_FAILURE = "get_contacts_failure",
+
+  //GET /api/contact
+  GET_CONTACT = "get_contact",
 }
