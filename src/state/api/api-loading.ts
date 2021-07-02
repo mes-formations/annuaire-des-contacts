3d3570f3b0c65ba28bbd078c6e16c35c0618b97c
Christian Lisangola <jochri3@gmail.com>
2021-07-02 21:43:22 +0100

    add loading reducer

---
+++ src/state/api/api-loading.ts
@@ -0,0 +1,7 @@
+export const loadingReducer = (state: any, action: any) => {
+  const { type } = action;
+  const matches = /(.*)_(request|success|failure)/.exec(type);
+  if (!matches) return state;
+  const [, requestName, requestStatus] = matches;
+  return { ...state, [requestName]: requestStatus === "request" };
+};
--- src/state/root.reducer.ts
+++ src/state/root.reducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { contactsReducer } from "./contacts/contacts.reducer";
+import { loadingReducer } from "./api/api-loading";
 
 const rootReducer = combineReducers({
   contacts: contactsReducer, //Les reducers sont des fonctions
+  apiLoading: loadingReducer,
 });
 
 export default rootReducer;
