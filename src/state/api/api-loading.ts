db3274f82bfa6d091292531c3e32e650658ab7ef
Christian Lisangola <jochri3@gmail.com>
2021-07-02 23:18:04 +0100

    add static typing in api-loading reducer

--- src/state/api/api-loading.ts
+++ src/state/api/api-loading.ts
@@ -1,4 +1,22 @@
-export const loadingReducer = (state: any = {}, action: any) => {
+import { ActionTypes } from "../contacts/contacts.action-types";
+import {
+  IGetContactRequest,
+  IGetContactsRequest,
+} from "../contacts/contacts.actions";
+
+// type LoadingState = Record<
+//   ActionTypes.GET_CONTACT | ActionTypes.GET_CONTACTS,
+//   boolean
+// >;
+
+type LoadingState = { [key in ActionTypes]?: boolean };
+
+type Actions = IGetContactRequest | IGetContactsRequest;
+
+export const loadingReducer = (
+  state: LoadingState,
+  action: Actions
+): LoadingState => {
   const { type } = action;
   const matches = /(.*)_(request|success|failure)/.exec(type);
   if (!matches) return state;
