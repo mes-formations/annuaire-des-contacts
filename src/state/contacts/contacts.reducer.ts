1acb720aaa7af3e3a07f62152b00c313a9f50e9d
Christian Lisangola <jochri3@gmail.com>
2021-07-03 12:22:59 +0100

    remove error state from contacts reducer

--- src/state/contacts/contacts.reducer.ts
+++ src/state/contacts/contacts.reducer.ts
@@ -5,13 +5,11 @@ import { IContact } from "../../interfaces/i-contact";
 export interface IContactsState {
   contacts: Map<string, IContact> | null;
   contact: IContact | null;
-  error: string | null;
 }
 
 const initialState: IContactsState = {
   contacts: new Map<string, IContact>(),
   contact: null,
-  error: "",
 };
 
 export const contactsReducer = (
@@ -21,12 +19,12 @@ export const contactsReducer = (
   // GET /contacts
 
   if (action.type === ActionTypes.GET_CONTACTS_SUCESS) {
-    return { ...state, error: null, contacts: action.payload };
+    return { ...state, contacts: action.payload };
   }
 
   // GET /contacts:id
   if (action.type === ActionTypes.GET_CONTACT_SUCCESS) {
-    return { ...state, error: null, contact: action.payload };
+    return { ...state, contact: action.payload };
   }
 
   return state;
