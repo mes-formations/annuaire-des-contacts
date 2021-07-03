f6d115c878e3cae5f7d207481bbc3ed65244ad9f
Christian Lisangola <jochri3@gmail.com>
2021-07-03 12:06:28 +0100

    add types in contacts selector

--- src/state/contacts/contacts.selectors.ts
+++ src/state/contacts/contacts.selectors.ts
@@ -1,14 +1,17 @@
-import { createSelector } from "reselect";
+import { RootState } from "../root.reducer";
+import { ActionTypes } from "./contacts.action-types";
 
 // Select all contacts
-export const selectContacts = (state: any) => state.contacts.contacts;
+export const selectContacts = (state: RootState) => state.contacts.contacts;
 
 //   select single contact
-export const selectContact = (state: any) => state.contacts.contact;
+export const selectContact = (state: RootState) => state.contacts.contact;
 
 // Common selectors
-export const selectContactsLoading = (actionType: any) => (state: any) =>
-  state.apiLoading[actionType];
+export const selectContactsLoading =
+  (actionType: ActionTypes) => (state: RootState) =>
+    state.apiLoading[actionType];
 
-export const selectContactsError = (actionType: any) => (state: any) =>
-  state.apiError[actionType];
+export const selectContactsError =
+  (actionType: ActionTypes) => (state: RootState) =>
+    state.apiError[actionType];
