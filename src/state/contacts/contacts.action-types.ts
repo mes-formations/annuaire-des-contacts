4596de8c2f9b5d2b7aea71dc7d2ddb4e1252c407
Christian Lisangola <jochri3@gmail.com>
2021-07-05 12:49:22 +0100

    add action types for delete

--- src/state/contacts/contacts.action-types.ts
+++ src/state/contacts/contacts.action-types.ts
@@ -2,6 +2,7 @@ export enum ActionTypes {
   // Main actions
   GET_CONTACTS = "get_contacts",
   GET_CONTACT = "get_contact",
+  DELETE_CONTACT = "delete_contact",
   // GET /api/contacts
   GET_CONTACTS_REQUEST = "get_contacts_request",
   GET_CONTACTS_SUCESS = "get_contacts_success",
@@ -11,4 +12,9 @@ export enum ActionTypes {
   GET_CONTACT_REQUEST = "get_contact_request",
   GET_CONTACT_SUCCESS = "get_contact_success",
   GET_CONTACT_FAILURE = "get_contact_failure",
+
+  // DELETE /api/contacts/:id
+  DELETE_CONTACT_REQUEST = "delete_contact_request",
+  DELETE_CONTACT_SUCCESS = "delete_contact_success",
+  DELETE_CONTACT_FAILURE = "delete_contact_failure",
 }
