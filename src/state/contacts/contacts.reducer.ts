06d54903d95fc2b9ece5362e53a09af86bc77e69
Christian Lisangola <jochri3@gmail.com>
2021-07-05 13:07:27 +0100

    add delete case in contacts reducer

--- src/state/contacts/contacts.reducer.ts
+++ src/state/contacts/contacts.reducer.ts
@@ -3,7 +3,7 @@ import { Action } from "./contacts.actions";
 import { IContact } from "../../interfaces/i-contact";
 
 export interface IContactsState {
-  collection: Map<string, IContact> | null;
+  collection: Map<string, IContact>;
   selected: IContact | null;
 }
 
@@ -16,15 +16,22 @@ export const contactsReducer = (
   state: IContactsState = initialState,
   action: Action
 ): IContactsState => {
-  // GET /contacts
+  // GET /api/contacts
 
   if (action.type === ActionTypes.GET_CONTACTS_SUCESS) {
     return { ...state, collection: action.payload };
   }
 
-  // GET /contacts:id
+  // GET /api/contacts/:id
   if (action.type === ActionTypes.GET_CONTACT_SUCCESS) {
     return { ...state, selected: action.payload };
+  }
+
+  // DELETE /api/contacts/:id
+  if (action.type === ActionTypes.DELETE_CONTACT_SUCCESS) {
+    const updatedContactsList = new Map<string, IContact>(state.collection);
+    updatedContactsList.delete(action.payload.id);
+    return { ...state, collection: updatedContactsList };
   }
 
   return state;
