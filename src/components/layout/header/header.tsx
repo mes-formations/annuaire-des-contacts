b458e5b7de9f46290a5b48f7fa96c86225856c6a
Christian Lisangola <jochri3@gmail.com>
2021-07-09 18:40:13 +0100

    add dynamic routes in header component

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { PAGES } from "../../../configs/pages";
 import "./styles.scss";
 
 const Header: React.FC = () => {
@@ -7,19 +8,19 @@ const Header: React.FC = () => {
       <nav className="nav">
         <ul className="nav-item nav-brand">
           <li>
-            <Link to="/" className="nav-link" href="#">
+            <Link to={PAGES.ROOT} className="nav-link" href="#">
               Accueil
             </Link>
           </li>
         </ul>
         <ul className="nav-item nav-links">
           <li>
-            <Link to="/contacts" className="nav-link" href="#">
+            <Link to={PAGES.CONTACTS_LIST} className="nav-link" href="#">
               Contacts
             </Link>
           </li>
           <li>
-            <Link to="#" className="nav-link" href="#">
+            <Link to={PAGES.CREATE_CONTACT_FORM} className="nav-link" href="#">
               Créer contact
             </Link>
           </li>
