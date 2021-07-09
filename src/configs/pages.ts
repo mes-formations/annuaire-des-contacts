fbd0c879d0b87ec2d53c919ea67c94695d5c2fa2
Christian Lisangola <jochri3@gmail.com>
2021-07-09 18:36:37 +0100

    add pages enum

---
+++ src/configs/pages.ts
@@ -0,0 +1,8 @@
+export enum PAGES {
+  ROOT = "/",
+  HOME = "home",
+  CREATE_CONTACT_FORM = "/contacts/new",
+  EDIT_CONTACT_FORM = "/contacts/:id/edit",
+  CONTACT_DETAILS = "/contacts/:id",
+  CONTACTS_LIST = "/contacts",
+}
