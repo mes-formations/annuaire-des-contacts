ee9471c1e1dca4fb3c9b77574afac1a81206922c
Christian Lisangola <jochri3@gmail.com>
2021-07-09 18:46:23 +0100

    add dynmaic enum router to app router

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Layout from "./components/layout/layout";
 import { LoaderRounded } from "./components/shared/loading-rounded/loader-rounded";
+import { PAGES } from "./configs/pages";
 
 const Homepage = lazy(() =>
   import(/* webpackChunkName: "home" */ "./pages/home/home")
@@ -24,13 +25,17 @@ const App: React.FC = () => {
     <Layout>
       <Suspense fallback={<LoaderRounded />}>
         <Switch>
-          <Route exact path="/home">
-            <Redirect to="/" />
+          <Route exact path={PAGES.HOME}>
+            <Redirect to={PAGES.ROOT} />
           </Route>
-          <Route exact path="/" component={Homepage} />
-          <Route exact path="/contacts" component={ContactsList} />
-          <Route exact path="/contacts/new" component={ContactCreate} />
-          <Route exact path="/contacts/:id" component={ContactShow} />
+          <Route exact path={PAGES.ROOT} component={Homepage} />
+          <Route exact path={PAGES.CONTACTS_LIST} component={ContactsList} />
+          <Route
+            exact
+            path={PAGES.CREATE_CONTACT_FORM}
+            component={ContactCreate}
+          />
+          <Route exact path={PAGES.CONTACT_DETAILS} component={ContactShow} />
         </Switch>
       </Suspense>
     </Layout>
--- src/configs/pages.ts
+++ src/configs/pages.ts
@@ -1,6 +1,6 @@
 export enum PAGES {
   ROOT = "/",
-  HOME = "home",
+  HOME = "/home",
   CREATE_CONTACT_FORM = "/contacts/new",
   EDIT_CONTACT_FORM = "/contacts/:id/edit",
   CONTACT_DETAILS = "/contacts/:id",
