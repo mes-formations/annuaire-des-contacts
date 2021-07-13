68d0873eb653bf3b3fd47a3d2629888d0f7e6238
Christian Lisangola <jochri3@gmail.com>
2021-07-13 18:25:27 +0100

    add immer in contacts reducer

--- src/state/contacts/contacts.reducer.ts
+++ src/state/contacts/contacts.reducer.ts
@@ -1,6 +1,7 @@
 import { ActionTypes } from "./contacts.action-types";
 import { Action } from "./contacts.actions";
 import { IContactResponse } from "../../interfaces/i-contact";
+import { produce } from "immer";
 
 export interface IContactsState {
   collection: Map<string, IContactResponse>;
@@ -12,31 +13,30 @@ const initialState: IContactsState = {
   selected: null,
 };
 
-export const contactsReducer = (
-  state: IContactsState = initialState,
-  action: Action
-): IContactsState => {
-  // GET /api/contacts
+export const contactsReducer = produce(
+  (state: IContactsState, action: Action): IContactsState => {
+    // GET /api/contacts
 
-  if (action.type === ActionTypes.GET_CONTACTS_SUCESS) {
-    return { ...state, collection: action.payload };
-  }
+    if (action.type === ActionTypes.GET_CONTACTS_SUCESS) {
+      state.collection = new Map<string, IContactResponse>(action.payload);
+      return state;
+    }
 
-  // GET /api/contacts/:id
-  if (action.type === ActionTypes.GET_CONTACT_SUCCESS) {
-    return { ...state, selected: action.payload };
-  }
+    // GET /api/contacts/:id
+    if (action.type === ActionTypes.GET_CONTACT_SUCCESS) {
+      state.selected = action.payload;
+      return state;
+    }
 
-  // DELETE /api/contacts/:id
-  if (action.type === ActionTypes.DELETE_CONTACT_SUCCESS) {
-    const updatedContactsList = new Map<string, IContactResponse>(
-      state.collection
-    );
-    updatedContactsList.delete(action.payload.id);
-    return { ...state, collection: updatedContactsList };
-  }
+    // DELETE /api/contacts/:id
+    if (action.type === ActionTypes.DELETE_CONTACT_SUCCESS) {
+      state.collection.delete(action.payload.id);
+      return state;
+    }
 
-  //POST /api/contacts : Ici ça dépend de l'action.Si par exemple, on veut créer un contact, on doit créer un objet IContactResponse et le pousser dans le store.
+    //POST /api/contacts : Ici ça dépend de l'action.Si par exemple, on veut créer un contact, on doit créer un objet IContactResponse et le pousser dans le store.
 
-  return state;
-};
+    return state;
+  },
+  initialState
+);
--- src/state/root.reducer.ts
+++ src/state/root.reducer.ts
@@ -4,6 +4,11 @@ import { apiLoadingReducer } from "./api/api-loading-reducer";
 import { apiErrorReducer } from "./api/api-error-reducer";
 import { formReducer } from "./contact-form/form.reducer";
 
+// The following 2 lines are needed to delete data from HashMap usinf immer
+import { enableMapSet } from "immer";
+
+enableMapSet();
+
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   apiLoading: apiLoadingReducer,
