5f31a95b53033b96cc5db447db765fb044a17d3f
Christian Lisangola <jochri3@gmail.com>
2021-07-13 18:11:45 +0100

    add immer to form reducer

--- src/state/contact-form/form.reducer.ts
+++ src/state/contact-form/form.reducer.ts
@@ -1,11 +1,9 @@
 import { ActionTypes } from "./form.action-types";
-import { ActionTypes as ContactActionTypes } from "../contacts/contacts.action-types";
 import { IContact } from "../../interfaces/i-contact";
 import { Action } from "./form.actions";
-import { Action as ContactAction } from "../contacts/contacts.actions";
-import { IContactsState } from "../contacts/contacts.reducer";
+import { produce } from "immer";
 
-const initialState: IContact = {
+const initialState = {
   first_name: "",
   last_name: "",
   email: "",
@@ -14,19 +12,16 @@ const initialState: IContact = {
   work_address: "",
 };
 
-export const formReducer = (
-  state: IContact = initialState,
-  action: Action
-): IContact => {
-  switch (action.type) {
-    case ActionTypes.UPDATE_CONTACT_FORM:
-      return {
-        ...state,
-        [action.payload.target.name]: action.payload.target.value,
-      };
-    case ActionTypes.RESET_CONTACT_FORM:
+export const formReducer = produce(
+  (state = initialState, action: Action): IContact => {
+    if (action.type === ActionTypes.UPDATE_CONTACT_FORM) {
+      state[action.payload.target.name] = action.payload.target.value;
+      return state;
+    }
+    if (action.type === ActionTypes.RESET_CONTACT_FORM) {
       return initialState;
-    case ActionTypes.POPULATE_EDIT_FORM:
+    }
+    if (action.type === ActionTypes.POPULATE_EDIT_FORM)
       return {
         ...state,
         first_name: action.payload?.first_name || "",
@@ -36,7 +31,7 @@ export const formReducer = (
         position: action.payload?.position || "",
         work_address: action.payload?.work_address || "",
       };
-    default:
-      return state;
-  }
-};
+    return state;
+  },
+  initialState
+);
